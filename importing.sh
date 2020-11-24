949d606f5b7108b2d550cdcd388decb6d89bbee2
marlen99 <marlen.ber@mail.ru>
2020-11-24 22:48:58 +0500

    corrected typo

--- importing.sh
+++ importing.sh
@@ -1,1 +1,1 @@
-neo4j-admin import --database arxiv --nodes=Authors=authors.csv --nodes=Article=articles.csv --relationships=WROTE=wrote.csv
+neo4j-admin import --database arxiv --nodes=Author=authors.csv --nodes=Article=articles.csv --relationships=WROTE=wrote.csv
