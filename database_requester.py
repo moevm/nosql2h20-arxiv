4c424ca8fdec08ec64e4a3adf2f7ec63e67f522a
marlen99 <marlen.ber@mail.ru>
2020-11-24 22:54:19 +0500

    fixed type in requests

--- database_requester.py
+++ database_requester.py
@@ -7,20 +7,21 @@ class DatabaseRequester:
 
     def get_authors_id(self, reg_name):
         with self.driver.session() as session:
-            res = session.run("MATCH (a:Authors) where a.name =~ $reg_name"
-                              " RETURN id(a)", reg_name=reg_name)
+            res = session.run("MATCH (a:Author)"
+                              " WHERE a.name =~ $reg_name"
+                               " RETURN id(a)", reg_name=reg_name)
             return res.value()
 
     def get_author_articles_id(self, author_id):
         with self.driver.session() as session:
-            res = session.run("MATCH (a:Authors)-[:WROTE]-(b:Article)"
+            res = session.run("MATCH (a:Author)-[:WROTE]-(b:Article)"
                               " WHERE id(a) = $author_id"
-                              " RETURN id(b)", author_id=author_id)
+                               " RETURN id(b)", author_id=author_id)
             return res.value()
 
 if __name__ == "__main__":
     req = DatabaseRequester("neo4j://localhost:7687", "neo4j", "password")
-    ids = req.get_authors_id("X.*")
+    ids = req.get_authors_id("Xu*")
     ids = req.get_author_articles_id(ids[0])
     print(ids)
 
