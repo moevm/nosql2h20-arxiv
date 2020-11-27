625607261dd895ec6a8e8177d7a2b54830aa2e5f
Kate <Kate.tyan57@gmail.com>
2020-11-27 19:24:57 +0300

    added co_author link to routes

--- frontend/src/utils/Routes.js
+++ frontend/src/utils/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Route, Switch} from "react-router-dom";
 import Home from "../pages/Home";
 import Author from "../pages/Author";
+import Co_author from "../pages/Co_authors";
 import Author_page from "../pages/Author_page";
 import Article_page from "../pages/Article_page";
 import Category from "../pages/Category";
@@ -16,6 +17,7 @@ export default function Routes() {
             <Route path="/article" component={Article}/>
             <Route path="/category" component={Category}/>
             <Route path="/author" component={Author}/>
+            <Route path="/co_author/:co_author" component={Co_author}/>
             <Route path="/author_page/:author_page" component={Author_page} />
             <Route path="/article_page/:article_page" component={Article_page} />
         </Switch>
