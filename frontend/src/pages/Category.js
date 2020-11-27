43ddc6a5b5f3effc3101d316c827eaa1a75918b9
Kate <Kate.tyan57@gmail.com>
2020-11-27 18:23:16 +0300

    added functional to category page

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -1,18 +1,92 @@
-import React, { useState } from "react";
-import {InputGroup,FormControl} from "react-bootstrap";
+import React, {useState, useEffect, createRef} from "react";
+import {InputGroup, FormControl, Table} from "react-bootstrap";
 import '../App.css';
 
 export default function Category() {
     const [category, setCategory] = useState('');
-    console.log(category)
-    return (
-        <div>
-            <InputGroup size="lg" onChange={(e) => setCategory(e.target.value)}>
-                <InputGroup.Prepend>
-                    <InputGroup.Text id="category">By category</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
-            </InputGroup>
-        </div>
-    );
+    const [error, setError] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [result, setResult] = useState('');
+    const [json, setJson] = useState([]);
+    let inform = createRef();
+    // Примечание: пустой массив зависимостей [] означает, что
+    // этот useEffect будет запущен один раз
+    // аналогично componentDidMount()
+
+    const handleChange = () => {
+        //console.log('AAAAA')
+        const inf = inform.current;
+        if (inf !== null) {
+            //console.log(inf.value)
+            setCategory(inf.value)
+        }
+    }
+
+    useEffect(() => {
+        //console.log('fetch',author)
+        fetch(`http://localhost:5000/category?category=${category}`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setJson(result);
+                },
+                // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+                // чтобы не перехватывать исключения из ошибок в самих компонентах.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [category])
+
+    const columns = [
+        {
+            dataField: "id",
+            text: 'Author id'
+        },
+        {
+            dataField: "name",
+            text: 'Author name'
+        },
+        {
+            dataField: "article",
+            text: 'Article name'
+        }
+    ];
+    if (error) {
+        return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Загрузка...</div>;
+    } else {
+        //console.log(author)
+        //console.log(json)
+        return (
+            <div>
+                <input ref={inform}/>
+                <button onClick={handleChange}>Search</button>
+                {json.map((json_value)=>(
+                    <h3>{json_value.category_name}</h3>
+                ))}
+                <Table>
+                    <thead>
+                        <tr>
+                            {columns.map((value, index) => (
+                                <th key={index}>{value.text}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {json.map((json_value, index) => (
+                            <tr key={index}>
+                                <th>{json_value.id}</th>
+                                <th><a href={'http://localhost:3000/author_page/'+json_value.author_name}>{json_value.author_name}</a></th>
+                                <th><a href={'http://localhost:3000/article_page/'+json_value.article_name}>{json_value.article_name}</a></th>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
 }
