6b1f697eee12bcf7c043ddd9005aaa1d525fc4d1
GizzatovAmir <gizzatovamir777@gmail.com>
2020-11-28 21:44:31 +0300

    added response for stat page

--- backend/utils/statistics.py
+++ backend/utils/statistics.py
@@ -1,65 +1,61 @@
 from flask_restful import reqparse, abort, Resource
 from flask import jsonify, make_response
 from .database_requester import DatabaseRequester
-
+import random as rdm
 parser = reqparse.RequestParser()
 parser.add_argument('author', location='args')
-data = {
-    'labels': [
-        'Red',
-        'Green',
-        'Yellow'
-    ],
-    'datasets': [{
-        'data': [300, 50, 100],
-        'backgroundColor': [
-            '#FF6384',
-            '#36A2EB',
-            '#FFCE56'
-        ],
-        'hoverBackgroundColor': [
-            '#FF6384',
-            '#36A2EB',
-            '#FFCE56'
-        ]
-    }]
-}
 
-bar_data = {
-    'labels': ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    'datasets': [
-        {
-            'label': 'Authors',
-            'data': [12, 19, 3, 5, 10, 3],
-            'backgroundColor':[
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-            ],
-            'borderColor': [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-            ],
-            'borderWidth': 1,
-        },
-    ],
-}
 
-#def get_author(author,req):
-#    retrun []
+def get_category_stat(req):
+    stat = req.get_categories_statistics()
+    labels = []
+    stat_data = []
+    color = []
+    for par in stat:
+        labels.append(par[0])
+        stat_data.append(par[1])
+    print(labels)
+    print(stat_data)
+    for i in range(len(stat_data)):
+        color.append([
+            'rgba({},{},{}, 1)'.format(rdm.randint(0,255),rdm.randint(0,255),rdm.randint(0,255))
+        ])
+    return {
+                 'labels': labels,
+                 'datasets': [{
+                    'label':'Categories',
+                     'data': stat_data,
+                     'backgroundColor': color
+                 }]
+             }
 
+def get_get_authors_stat(req):
+    stat = req.get_authors_statistics()
+    labels = []
+    stat_data = []
+    color = []
+    for par in stat:
+        labels.append(par[0])
+        stat_data.append(par[1])
+    print(labels)
+    print(stat_data)
+    names = req.get_authors_names(labels)
+    for i in range(len(stat_data)):
+        color.append([
+            'rgba({},{},{}, 1)'.format(rdm.randint(0,255),rdm.randint(0,255),rdm.randint(0,255))
+        ])
+    return {
+                 'labels': names,
+                 'datasets': [{
+                     'data': stat_data,
+                     'backgroundColor': color
+                 }]
+             }
 class Stat(Resource):
-
     def get(self):
         req = DatabaseRequester("bolt://localhost:7687", "neo4j", "password")
-
+        bar_data = get_category_stat(req)
+        data = get_get_authors_stat(req)
         return make_response(
             jsonify(data,bar_data),
             200)
