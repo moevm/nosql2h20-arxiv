edc7d5bd6b8ab2c0bae6e41c17b2ba2a8190af5d
GizzatovAmir <gizzatovamir777@gmail.com>
2020-11-28 21:42:59 +0300

    added response for coauthor page

--- backend/utils/co_author.py
+++ backend/utils/co_author.py
@@ -3,18 +3,22 @@ from flask import jsonify, make_response
 from .database_requester import DatabaseRequester
 
 parser = reqparse.RequestParser()
-parser.add_argument('co_authors', location='args')
+parser.add_argument('co_author', location='args')
 
 def get_co_author(co_authors,req):
-    answer = [{
-        "author_name":co_authors,
-        'co_authors':[
-        {'name':"Amir",'article_name':'kek'},
-        {'name':"Marlen",'article_name':'kek'},
-        {'name':"Kate",'article_name':'kek'},
-        {'name':"Kate",'article_name':'kek'},
-        {'name':"Kate",'article_name':'kek'}
-        ]
+    name = req.get_authors_names([int(co_authors)])
+    print(name)
+    ids_of_co_authors = req.get_colleagues_of_author(int(co_authors))
+    print(ids_of_co_authors)
+    names_of_co_authors = req.get_authors_names(ids_of_co_authors)
+    result=[]
+    for author_name,author_id in zip(names_of_co_authors,ids_of_co_authors):
+        result.append({'author_name':author_name,'author_id':author_id})
+    print(result)
+    answer=[{
+    'author_name':name,
+    'author_id':co_authors,
+    'co_authors':result
     }]
 
     return answer
@@ -25,7 +29,7 @@ class CoAuthors(Resource):
         req = DatabaseRequester("bolt://localhost:7687", "neo4j", "password")
         args = parser.parse_args()
         print(args)
-        co_authors = args.get('co_authors')
+        co_authors = args.get('co_author')
         answer = get_co_author(co_authors,req)
         return make_response(
             jsonify(answer),
