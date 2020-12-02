01b1ffd6469678f2be3fe1c0805b39ddff5cbf2b
GizzatovAmir <gizzatovamir777@gmail.com>
2020-12-02 19:50:12 +0300

    build and docker-compose fix

--- build.sh
+++ build.sh
@@ -3,22 +3,22 @@
 if [[ -f db/arxiv.tar.gz ]]
 then
     echo 'dataset is already downloaded'
-#else
-#    bash db/wget_arxiv.sh
-#    tar -xf arxiv.tar.gz
+else
+    bash db/wget_arxiv.sh
+    tar -xf arxiv.tar.gz
 fi
-#if [[ -d /var/lib/neo4j/data/databases/neo4j ]]
-#then
-#    echo 'AAAAAAAAAAAA'
-#    sudo rm -r /var/lib/neo4j/data/databases/neo4j
-#fi
-#if [[ -d /var/lib/neo4j/data/transactions/neo4j ]]
-#then
-#    sudo rm -r /var/lib/neo4j/data/transactions/neo4j
-#fi
+if [[ -d /var/lib/neo4j/data/databases/neo4j ]]
+then
+    echo 'AAAAAAAAAAAA'
+    sudo rm -r /var/lib/neo4j/data/databases/neo4j
+fi
+if [[ -d /var/lib/neo4j/data/transactions/neo4j ]]
+then
+    sudo rm -r /var/lib/neo4j/data/transactions/neo4j
+fi
 
-#bash db/importing.sh
-#sudo neo4j start
+bash db/importing.sh
+sudo neo4j start
 
 docker-compose build --no-cache
 docker-compose up
